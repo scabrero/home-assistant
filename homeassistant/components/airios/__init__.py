9e0c90d6506ceb02f64c6562b4d3705eacb9b257
Samuel Cabrero <samuel@orica.es>
2025-03-19 21:35:58 +0100

    Button platform for Airios integration

--- homeassistant/components/airios/__init__.py
+++ homeassistant/components/airios/__init__.py
@@ -27,6 +27,7 @@ _LOGGER = logging.getLogger(__name__)
 
 PLATFORMS: list[Platform] = [
     Platform.BINARY_SENSOR,
+    Platform.BUTTON,
     Platform.FAN,
     Platform.NUMBER,
     Platform.SELECT,
