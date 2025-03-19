b983574a90efe69b286db9d211e9029384d9226a
Samuel Cabrero <samuel@orica.es>
2025-03-19 21:33:59 +0100

    Number platform for Airios integration
    Signed-off-by: Samuel Cabrero <samuel@orica.es>

--- homeassistant/components/airios/__init__.py
+++ homeassistant/components/airios/__init__.py
@@ -28,6 +28,7 @@ _LOGGER = logging.getLogger(__name__)
 PLATFORMS: list[Platform] = [
     Platform.BINARY_SENSOR,
     Platform.FAN,
+    Platform.NUMBER,
     Platform.SELECT,
     Platform.SENSOR,
 ]
